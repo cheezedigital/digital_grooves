cebca526e346f22480f1971e766066cf6b72f2c9
lcbunch <lbunch@vt.edu>
2014-06-14 11:12:38 -0600

    edit stuff

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
     def new
       @items = Item.all
     end
 
     def show
-      @item = Item.find(params[:id])
     end
 
+    def edit
+    end
 
-
+    def update
+      if @item.update_attributes(item_params)
+        redirect_to products_path
+      else
+        render :edit
+      end
+    end
+    
     private
+
+    def find_item
+      @item = Item.find(params[:id])
+    end
 end
