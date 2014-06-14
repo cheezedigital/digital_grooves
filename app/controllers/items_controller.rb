96e4d51e4206a93cac7237f1a76f901407373349
Justin Veirs <justinveirs@gmail.com>
2014-06-14 11:08:20 -0600

    added controller actions, and updated routes to reflect configuration

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,16 @@
 class ItemsController < ApplicationController
 
-  
+  def create
+    @item = Item.create
+  end
+
+  def new
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render.new 
+  end
+
+
 end
