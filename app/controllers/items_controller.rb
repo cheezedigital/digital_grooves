25066ef9b62db28007af7b9e4d1ca266494984e1
Skewampus <herrogoodbye24@gmail.com>
2014-06-14 11:01:37 -0600

    added items folder and added new/show actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
+    def new
+      @items = Item.all
+    end
 
-  
+    def show
+      @item = Item.find(params[:id])
+    end
+
+
+
+    private
 end
